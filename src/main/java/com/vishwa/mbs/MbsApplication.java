13b9ed8d0981d8e9f3c34ef815fe1af69811b161
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2021-10-10 19:21:32 +0530

    added the code

---
+++ src/main/java/com/vishwa/mbs/MbsApplication.java
@@ -0,0 +1,18 @@
+package com.vishwa.mbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class of the SpringBoot
+ */
+@SpringBootApplication
+public class MbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbsApplication.class, args);
+
+		System.out.println("Hello Studnets ");
+	}
+
+}
